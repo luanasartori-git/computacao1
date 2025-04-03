742144b702114bcfadfb713cb2d97f62800fa2d4
luanasartori-git <luanarichasartori@gmail.com>
2025-04-03 16:35:27 -0300

    Add files via upload

---
+++ médias e classificações.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+
+    float n1, n2, n3, media;
+
+    printf("Digite as tres notas: ");
+    scanf("%f %f %f", &n1, &n2, &n3);
+
+    media = (n1 + n2 + n3) /3;
+
+    if(media > 8)
+        printf("Aprovado com Distincao");
+    else if(media >= 6)
+        printf("Aprovado Direto");
+    else if(media >= 4)
+        printf("Vai para Final");
+    else
+        printf("Reprovado Direto");
+
+    return 0;
+}
