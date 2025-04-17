e4cb991dd22f74b6e9e99f204de6459080306893
luanasartori-git <luanarichasartori@gmail.com>
2025-04-17 16:53:15 -0300

    Add files via upload

---
+++ Repetição parte II - 1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main ()
+{
+    float numero,
+          maior_numero;
+
+    printf("Digite um numero: ");
+    scanf("%f", &numero);
+
+    maior_numero = numero;
+
+    while(numero >= 0)
+    {
+        if(numero>maior_numero)(maior_numero = numero);
+
+        printf("Digite mais um numero: ");
+        scanf("%f", &numero);
+
+        printf("maior_numero = %f\n", maior_numero);
+
+    }
+    return 0;
+}
