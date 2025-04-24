3e70092db9555a80d0bdb250f3b1400ed9360265
luanasartori-git <luanarichasartori@gmail.com>
2025-04-24 16:10:00 -0300

    Add files via upload

---
+++ recuperação q1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+        float altura, largura;
+
+    printf("Digite a altura(em metros): ");
+    scanf("%f", &altura);
+
+    printf("Digite a largura(em metros): ");
+    scanf("%f", &largura);
+
+    return 0;
+}
+
